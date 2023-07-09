ff0727330c2127bce489f15856bdc9a1a16898e3
waddaguma <xanux@naver.com>
2023-07-09 15:14:11 +0900

    test

---
+++ src/main/java/com/pcm/wadda01/test01controller/Test01Controller.java
@@ -0,0 +1,15 @@
+package com.pcm.wadda01.test01controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test01")
+public class Test01Controller {
+
+	@GetMapping
+	public String hello() {
+		return "hello!!!";
+	}
+}
